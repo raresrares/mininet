2b96cf6dcc98cd9ad4156c98b7b11334a2ee29bd
raresrares <aurelian.stanca@stud.acs.upb.ro>
2023-04-04 14:59:16 +0300

    add hosts and links between them

--- topology.py
+++ topology.py
@@ -1,16 +1,29 @@
 from mininet.topo import Topo
-
-class TutorialTopology( Topo ):
-    def build( self ):
-        # add a host to the network
-        h1 = self.addHost( 'h1' )
-
-        # add a switch to the network
-        s1 = self.addSwitch( 's1' )
-
-        # add a link between the host `h1` and the `s1` switch
-        self.addLink( h1, s1 )
-
-    topos = { 'tutorialTopology': ( lambda: TutorialTopology() ) }
+from mininet.net import Mininet
+from mininet.link import TCLink
+from mininet.log import setLogLevel
 
 
+class NetworkTopo(Topo):
+    def build(self):
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+
+        # add three links between h1 and h2
+        for _ in range(3):
+            self.addLink(h1, h2)
+
+
+def run():
+    topo = NetworkTopo()
+
+    net = Mininet(topo=topo)
+
+    net.start()
+    net.pingAll()
+    net.interact()
+
+
+if __name__ == "__main__":
+    setLogLevel("info")
+    run()
