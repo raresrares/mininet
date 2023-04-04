f57b64a1f6f569e3589a4f644726fec10a974b93
raresrares <aurelian.stanca@stud.acs.upb.ro>
2023-04-04 16:02:15 +0300

    Changes

--- topology.py
+++ topology.py
@@ -1,23 +1,28 @@
+from mininet.node import CPULimitedHost
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.link import TCLink
 from mininet.log import setLogLevel
 
 
-class NetworkTopo(Topo):
+class Topology(Topo):
+
     def build(self):
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
 
-        # add two links between h1 and h2
-        for _ in range(2):
-            self.addLink(h1, h2)
+        s1 = self.addSwitch('s1')
+
+        self.addLink(h1, s1, cls=TCLink, delay='40ms', bw=8)
+        self.addLink(h2, s1, cls=TCLink, delay='40ms', bw=8)
+        self.addLink(h1, s1, cls=TCLink, delay='40ms', bw=8)
+        self.addLink(h2, s1, cls=TCLink, delay='40ms', bw=8)
 
 
 def run():
-    topo = NetworkTopo()
+    topo = Topology()
 
-    net = Mininet(topo=topo)
+    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
 
     net.start()
     net.pingAll()
